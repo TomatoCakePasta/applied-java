ad7aed611c4730e33071e42e229e458d54a51732
TomatoCakePasta <gaomuk50@gmail.com>
2026-07-16 15:41:31 +0900

    実装: enum型を追加

--- ch2/Main.java
+++ ch2/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // define class using generics
-        StrongBox<String> stBox = new StrongBox<>();
+        StrongBox<String> stBox = new StrongBox<>(KeyType.PADLOCK);
         
         stBox.put("Hello World");
         IO.println(stBox.get());
--- ch2/StrongBox.java
+++ ch2/StrongBox.java
@@ -1,11 +1,44 @@
 public class StrongBox<E> {
     private E data;
+    private KeyType keyType;
+    private long cnt;
+
+    public StrongBox(KeyType keyType) {
+        this.keyType = keyType;
+    }
     
     public void put(E data) {
         this.data = data;
     }
     
     public E get() {
+        cnt++;
+
+        switch (keyType) {
+            // Arrow構文でbreak省略可能
+            case PADLOCK -> {
+                if (cnt < 1024) {
+                    return null;
+                }
+            } 
+            case BUTTON -> {
+                if (cnt < 10000) {
+                    return null;
+                }
+            }
+            case DIAL -> {
+                if (cnt < 30000) {
+                    return null;
+                }
+            }
+            case FINGER -> {
+                if (cnt < 1000000L) {
+                    return null;
+                }
+            }
+        }
+
+        cnt = 0;
         return data;
     }
 }
