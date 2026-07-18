8ed1ccb0beca5f1274e26a29e3049714f58aae72
TomatoCakePasta <gaomuk50@gmail.com>
2026-07-18 09:29:15 +0900

    実装: StreamAPI

--- ch3/Main.java
+++ ch3/Main.java
@@ -12,5 +12,36 @@ public class Main {
         IntFunction<Boolean> func3 = (int x) -> { return (x % 2 == 1); };
         IO.println(func3.apply(2));
         IO.println(func3.apply(1));
+
+        // intを受け取ってbooleanを返す
+        // こっちの方がさらに良い
+        IntPredicate func4 = (int x) -> { return (x % 2 == 1); };
+        IO.println(func4.test(2));
+        IO.println(func4.test(1));
+
+        // StreamAPI
+        IO.println("===== StreamAPI =====");
+        List<String> peoples = new ArrayList<>();
+        peoples.add("佐々木姫華");
+        peoples.add("田中太郎");
+        peoples.add("山健吾");
+        
+        IO.println("\nBefore:");
+        printArrays(peoples);
+        
+        // streamに変換 -> 色々処理をする -> 再度リストに変換
+        List<String> convert_peoples = peoples.stream()
+                                        .filter(p -> p.length() <= 4)
+                                        .map(p -> p + "さん")
+                                        .collect(Collectors.toList());
+               
+        IO.println("\nAfter: 4文字以下の人に"さん"をつける);                         
+        printArrays(convert_peoples);
+    }
+
+    public static void printArrays(List<String> arr) {
+        for (String elem : arr) {
+            IO.println(elem);
+        }
     }
 }
